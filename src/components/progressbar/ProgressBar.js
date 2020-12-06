b6a4c02d9a3c6b3192e09657b9b460df8183c59c
junholee <juno7803@naver.com>
2020-12-07 04:01:31 +0900

    [DELETE] antd ProgressBar 삭제

--- src/components/progressbar/ProgressBar.js
+++
@@ -1,9 +0,0 @@
-import {Progress} from 'antd';
-
-function ProgressBar({percent}){
-    return(
-        <Progress style={{width:"50%"}}percent={percent} showInfo={true}/>
-    );
-}
-
-export default ProgressBar;
