5a15dfe8ab9cc08135df440af031cc69717d4e56
junholee <juno7803@naver.com>
2020-12-10 20:52:42 +0900

    [ADD] redux-persist 모듈 추가로 새로고침 대응

--- src/index.js
+++ src/index.js
@@ -11,14 +11,19 @@ import {createStore,applyMiddleware} from "redux";
 import rootReducer from "./store/index";
 import Thunk from 'redux-thunk';
 import {composeWithDevTools} from "redux-devtools-extension";
+import {persistStore} from "redux-persist";
+import {PersistGate} from "redux-persist/integration/react";
 
 const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(Thunk)));
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
+    <PersistGate persistor={persistor}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,18 @@
 import {combineReducers} from 'redux';
+import {persistReducer} from "redux-persist";
 import result from "./action/result";
 import survey from "./action/survey";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+    key: "root",
+    storage: storage,
+    whitelist: ["result","survey"],
+};
 
 const rootReducer = combineReducers({
     result,
     survey
 })
 
-export default rootReducer;
+export default persistReducer(persistConfig,rootReducer);
